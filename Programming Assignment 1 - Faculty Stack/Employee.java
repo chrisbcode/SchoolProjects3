774684daeb9e08d0ccc860cec0d291cc160be47b
Chris B <50760809+chrisbcode@users.noreply.github.com>
2025-09-29 18:59:12 -0400

    Update Employee.java

--- Programming Assignment 1 - Faculty Stack/Employee.java
+++ Programming Assignment 1 - Faculty Stack/Employee.java
@@ -1,7 +1,7 @@
 /**
  * @author Christopher Bergsveinsson
  * @version 9/23/2025
- * CSC 223 - Anwar Ahmad
+ * CSC 223 
  * Every Tuesday and  Thursday 9:35am → 11:35am
  * Programming Assignment 1 - Stack ADT program for Computer Science enrollment - Employee Class
  */
@@ -49,3 +49,4 @@ public class Employee extends Person {
     }
 
 }
+
