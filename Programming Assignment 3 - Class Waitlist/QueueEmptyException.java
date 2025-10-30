1a625a44933e24c8a1c6aa99fc050a9a2d4b924a
Chris B <50760809+chrisbcode@users.noreply.github.com>
2025-10-30 10:54:26 -0400

    Update QueueEmptyException.java

--- Programming Assignment 3 - Class Waitlist/QueueEmptyException.java
+++ Programming Assignment 3 - Class Waitlist/QueueEmptyException.java
@@ -1,7 +1,7 @@
 /**
  * @author Christopher Bergsveinsson
  * @version 10/30/2025
- * CSC 223 - Anwar Ahmad
+ * CSC 223
  * Every Tuesday and  Thursday 9:35am → 11:35am
  * Programming Assignment 3 - Queue ADT for class waitlist - QueueEmptyException
  */
@@ -16,3 +16,4 @@ public class QueueEmptyException extends Exception
         super(message);
     }
 }
+
