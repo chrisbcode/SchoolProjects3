70db4d633a2c15cbc618d83a5fd1e69c2e3aeb58
Chris B <50760809+chrisbcode@users.noreply.github.com>
2025-10-30 10:54:00 -0400

    Update ClassWaitlist.java

--- Programming Assignment 3 - Class Waitlist/ClassWaitlist.java
+++ Programming Assignment 3 - Class Waitlist/ClassWaitlist.java
@@ -1,7 +1,7 @@
 /**
  * @author Christopher Bergsveinsson
  * @version 10/30/2025
- * CSC 223 - Anwar Ahmad
+ * CSC 223
  * Every Tuesday and  Thursday 9:35am → 11:35am
  * Programming Assignment 3 - Queue ADT for class waitlist - ClassWaitlist
  */
@@ -79,3 +79,4 @@ public class ClassWaitlist<E> implements QueueInterface<E> {
 
 
 }
+
