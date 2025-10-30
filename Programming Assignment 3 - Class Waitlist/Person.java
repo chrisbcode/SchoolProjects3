67597b64a8bfd6df95e05a05c9ecf2cbbd2ddc45
Chris B <50760809+chrisbcode@users.noreply.github.com>
2025-10-30 10:54:17 -0400

    Update Person.java

--- Programming Assignment 3 - Class Waitlist/Person.java
+++ Programming Assignment 3 - Class Waitlist/Person.java
@@ -1,7 +1,7 @@
 /**
  * @author Christopher Bergsveinsson
  * @version 10/30/2025
- * CSC 223 - Anwar Ahmad
+ * CSC 223
  * Every Tuesday and  Thursday 9:35am → 11:35am
  * Programming Assignment 3 - Queue ADT for class waitlist - Person class
  */
@@ -39,3 +39,4 @@ public class Person {
     }
 
 }
+
