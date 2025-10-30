3b8a54bdb5470883f7e594077fb987a4a24ad49b
Chris B <50760809+chrisbcode@users.noreply.github.com>
2025-10-30 10:54:08 -0400

    Update ClassWaitlistDemo.java

--- Programming Assignment 3 - Class Waitlist/ClassWaitlistDemo.java
+++ Programming Assignment 3 - Class Waitlist/ClassWaitlistDemo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * @author Christopher Bergsveinsson
  * @version 10/30/2025
- * CSC 223 - Anwar Ahmad
+ * CSC 223
  * Every Tuesday and  Thursday 9:35am → 11:35am
  * Programming Assignment 3 - Queue ADT for class waitlist - ClassWaitlistDemo
  */
